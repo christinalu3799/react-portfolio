6de944aac45c8f7334c66f3f721517f1b3a4c43c
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-09-12 20:13:54 -0400

    added ease-in-out to button hover

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,21 +4,25 @@ import Headshot from '../assets/headshot.jpg'
 const Home = () => {
   return (
     <div name='home' className='flex w-full h-screen justify-center bg-[#ffffff] pt-[80px]'>
+
       {/* container */}
       <div className='flex flex-col-reverse h-full sm:flex-row max-w-[1000px] justify-center items-center'> 
 
         {/* hero */}
         <div className='w-full sm:w-8/12 px-8 sm:pl-18 sm:pr-0 mx-auto flex flex-col justify-center h-fit sm:h-full font-mono text-[#5e5e5e]'>
+
           <p className='mt-8'>Hi, my name is</p>
           <h1 className='text-5xl sm:text-6xl font-bold font-display py-4'>Christina Lu</h1>
           <h2 className='text-2xl sm:text-3xl font-bold'>I'm a Fullstack Developer.</h2>
           <div className='py-4'>
-            <button className='text-[#5e5e5e] group border px-6 py-3 my-2 flex items-center hover:bg-[#5e5e5e] hover:text-white hover:border-transparent rounded'>
+
+            {/* view my work button */}
+            <button className='text-[#5e5e5e] group border px-6 py-3 my-2 flex items-center hover:bg-[#5e5e5e] hover:text-white hover:border-transparent rounded transition ease-in-out duration-300'>
               View My Work 
-              <span className='group-hover:rotate-90 duration-300'><HiArrowNarrowRight className='ml-3' /></span>
+              <span className='group-hover:translate-x-[5px] transition ease-in-out duration-300'><HiArrowNarrowRight className='ml-3' /></span>
             </button>
+
           </div>
-        
         </div>
 
         {/* headshot */}
