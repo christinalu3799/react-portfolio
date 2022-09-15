e9a4819ab0173099e980b864f550236bbf16d0d8
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-09-15 08:41:56 -0400

    created build

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import About from './components/About'
 import Skills from './components/Skills';
 import Work from './components/Work';
 import Contact from './components/Contact';
-import ProjectCard from './components/ProjectCard';
 
 function App() {
   return (
