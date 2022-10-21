36b8c45d6ef24d9e575742b772510eb639c7b70e
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-10-20 20:17:44 -0400

    darkened input and project card outlines

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,11 +13,11 @@ const Contact = () => {
                 <p className='text-4xl font-bold inline border-b-4'>Contact</p>
                 <p className='py-8'>Feel free to reach out to me about anything or just to say hi!</p>
             </div>
-            <input className='p-2 border rounded outline-none' type="text" placeholder='Name' name='name' required />
-            <input className='my-4 p-2 border rounded outline-none' type="email" placeholder='Email' name='email' required />
-            <textarea className='p-2 border rounded outline-none resize-none' name="message" rows="10" placeholder='Message' required></textarea>
+            <input className='p-2 border border-[#c0c0c0] rounded outline-none' type="text" placeholder='Name' name='name' required />
+            <input className='my-4 p-2 border border-[#c0c0c0] rounded outline-none' type="email" placeholder='Email' name='email' required />
+            <textarea className='p-2 border border-[#c0c0c0] rounded outline-none resize-none' name="message" rows="10" placeholder='Message' required></textarea>
 
-            <button className='text-[#5e5e5e] border rounded border-[#5e5e5e] hover:bg-[#5e5e5e] hover:border-white hover:text-white px-4 py-3 my-8 mx-auto flex items-center transition ease-in-out duration-300'>Submit</button>
+            <button className='text-[#5e5e5e] border rounded border-[#c0c0c0] hover:bg-[#5e5e5e] hover:border-white hover:text-white px-4 py-3 my-8 mx-auto flex items-center transition ease-in-out duration-300'>Submit</button>
         </form>
     </div>
   )
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,7 +6,7 @@ class ProjectCard extends Component {
         console.log(this.state)
         return (
         
-            <div className='h-[400px] p-4 shadow-lg shadow-[#d6d6d6] group container rounded flex flex-col justify-center items-center mx-auto bg-[#ffffff] '>
+            <div className='h-[400px] border border-[#c0c0c0] p-4 shadow-lg shadow-[#d6d6d6] group container rounded flex flex-col justify-center items-center mx-auto bg-[#ffffff] '>
 
                 <div className='w-full h-full sm:h-[250px] bg-[#eeeeee]'> 
                     <img className='object-cover rounded w-full h-full shadow filter contrast-75 hover:contrast-100 ease-in-out duration-500' src={this.props.projectImg} alt={this.props.alt}/>
@@ -30,10 +30,10 @@ class ProjectCard extends Component {
                     {/* buttons */}
                     <div className='pt-2 text-center'>
                         <a href={this.props.demo} target='_blank' rel="noreferrer">
-                            <button className='text-sm text-center border rounded px-2 py-1 m-2 bg-white text-[#5e5e5e] font-bold hover:bg-[#5e5e5e] hover:text-[#ffffff] hover:border-[#5e5e5e] transition ease-in-out duration-300'>Demo</button>
+                            <button className='text-sm text-center border border-[#c0c0c0] rounded px-4 py-2 m-2 bg-white text-[#5e5e5e] font-bold hover:bg-[#5e5e5e] hover:text-[#ffffff] hover:border-[#5e5e5e] transition ease-in-out duration-300'>Demo</button>
                         </a>
                         <a href={this.props.code} target='_blank' rel="noreferrer">
-                            <button className='text-sm text-center border rounded px-2 py-1 m-2 bg-white text-[#5e5e5e] font-bold hover:bg-[#5e5e5e] hover:text-[#ffffff] hover:border-[#5e5e5e] transition ease-in-out duration-300'>Code</button>
+                            <button className='text-sm text-center border border-[#c0c0c0] rounded px-4 py-2 m-2 bg-white text-[#5e5e5e] font-bold hover:bg-[#5e5e5e] hover:text-[#ffffff] hover:border-[#5e5e5e] transition ease-in-out duration-300'>Code</button>
                         </a>
                     </div>
                 </div>
